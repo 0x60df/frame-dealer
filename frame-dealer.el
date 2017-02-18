1c23103630ed057f9f8976ff696e02deef7fde6f
0x60df <0x60df@gmail.com>
2017-02-18 09:27:01 +0900

    Initial commit

---
+++ frame-dealer.el
@@ -0,0 +1,36 @@
+;;; frame-dealer.el --- set frame position according to any dealing rule.
+
+
+;; Copyright (C) 2017 by 0x60DF
+
+;; Author: 0x60DF <0x60DF@gmail.com>
+;; Version: 0.0.0
+;; Keywords: frame
+;; URL: https://github.com/0x60df/frame-dealer
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; ‘frame-dealer’ provides a minor mode to set a window frame position
+;; according to any dealing rule when `make-frame'. As the rule, User can
+;; specify function which returns position where frame should be placed.
+
+;;; Code:
+
+(provide 'frame-dealer)
+
+;;; frame-dealer.el ends here
